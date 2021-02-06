433828db1768326f46cc7b1ae9bca7e18a3f50fe
Guillermo Naharro <sr.grillo79@gmail.com>
2021-02-06 13:00:09 +0100

    fixed peaceful despawn

---
+++ src/main/java/com/grillo78/littlecritters/mixin/MixinMonsterEntity.java
@@ -0,0 +1,24 @@
+package com.grillo78.littlecritters.mixin;
+
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.monster.MonsterEntity;
+import net.minecraft.entity.monster.SilverfishEntity;
+import net.minecraft.entity.passive.BeeEntity;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(MonsterEntity.class)
+public abstract class MixinMonsterEntity {
+
+    @Inject(method = "Lnet/minecraft/entity/monster/MonsterEntity;isDespawnPeaceful()Z", at = @At("RETURN"), cancellable = true)
+    public void isInRangeToRenderDist(CallbackInfoReturnable callbackInfoReturnable) {
+        if (((Object) this) instanceof SilverfishEntity) {
+            callbackInfoReturnable.setReturnValue(false);
+        }
+    }
+
+}
